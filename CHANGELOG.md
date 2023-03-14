51b34c51c9b502a56884f611c8fe69c31b80c1ec
Oscar Otero <oom@oscarotero.com>
2023-03-14 13:32:06 +0100

    new version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,16 @@
 <!-- deno-fmt-ignore-file -->
 
 # Changelog
-
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this
 project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.4.0] - 2023-03-14
+### Added
+- New `attribute` option to `image` plugin to select the first image
+  with a specific attribute (By default is `main`) instead of the first image found.
+  For example: `![alt](/image.png){main}`
 
 ## [0.3.0] - 2023-01-28
 ### Added
@@ -24,6 +29,7 @@ project adheres to [Semantic Versioning](http://semver.org/).
 - `toc` plugin.
 - `title` plugin.
 
+[0.4.0]: https://github.com/lumeland/markdown-plugins/compare/v0.3.0...v0.4.0
 [0.3.0]: https://github.com/lumeland/markdown-plugins/compare/v0.2.0...v0.3.0
 [0.2.0]: https://github.com/lumeland/markdown-plugins/compare/v0.1.0...v0.2.0
 [0.1.0]: https://github.com/lumeland/markdown-plugins/releases/tag/v0.1.0
--- image/mod.ts
+++ image/mod.ts
@@ -2,34 +2,59 @@
 export interface Options {
   /** Key to save the image in the page data */
   key: string;
+  attribute: string;
 }
 
 export const defaults: Options = {
   key: "image",
+  attribute: "main",
 };
 
 export default function image(md: any, userOptions: Partial<Options> = {}) {
   const options = Object.assign({}, defaults, userOptions) as Options;
 
-  function getImage(tokens: any[]): string | undefined {
+  function getImage(tokens: any[], img: PageImage): PageImage {
     for (const token of tokens) {
       if (token.type === "image") {
+        let src = "";
+        let main = false;
         for (const [name, value] of token.attrs) {
           if (name === "src") {
-            return value;
+            src = value;
+          }
+          if (name === options.attribute) {
+            main = true;
+          }
+        }
+        // Remove main attribute
+        const index = token.attrIndex(options.attribute);
+
+        if (index !== -1) {
+          token.attrs.splice(index, 1);
+        }
+
+        if (src) {
+          if (!img.first) {
+            img.first = src;
+          }
+          if (main) {
+            img.main = src;
+            return img;
           }
         }
         continue;
       }
 
       if (token.children) {
-        const image = getImage(token.children);
+        img = getImage(token.children, img);
 
-        if (image) {
-          return image;
+        if (img.main) {
+          return img;
         }
       }
     }
+
+    return img;
   }
 
   md.core.ruler.push("getImage", function (state: any) {
@@ -39,6 +64,12 @@ export default function image(md: any, userOptions: Partial<Options> = {}) {
       return;
     }
 
-    data[options.key] = getImage(state.tokens);
+    const img = getImage(state.tokens, {});
+    data[options.key] = img.main || img.first;
   });
+}
+
+interface PageImage {
+  first?: string;
+  main?: string;
 }
